7279d02f9502df6c9beb070975a5daaf0b700a78
Caesar-Kaslana <151535787+Caesar-Kaslana@users.noreply.github.com>
2024-10-14 21:17:31 +0800

    Add files via upload

---
+++ Data_Structure/lab4/SequenceQueue.h
@@ -0,0 +1,63 @@
+typedef struct 
+{
+    ElemType queue[MaxQueueSize];
+    int front;   /*队头指针*/
+    int count;  /*计数器*/
+}SequenceQueue;
+
+/*初始化循环队列Q*/
+void QueueInitiate(SequenceQueue *Q)
+{
+    Q->front = 0;
+    Q->count = 0;
+}
+
+/*判断队列是否为空*/
+int QueueNotEmpty(SequenceQueue Q) 
+{
+    /*空队列返回0，否则返回1*/
+    if (Q.count == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+/*入队列，在队尾插入元素x*/
+int QueueAppend(SequenceQueue *Q, ElemType x)
+{
+    /*成功入队返回1，否则返回0*/
+    if (Q->count == MaxQueueSize)
+    {
+        printf("Queue is Full!");
+        return 0;
+    }
+    else
+    {
+        Q->queue[(Q->front + Q->count) % MaxQueueSize] = x;
+        Q->count++;
+        return 1;
+    }
+}
+
+/*出队列，把队列Q的队头元素删除并由参数d带回。*/
+int QueueDelete(SequenceQueue *Q, ElemType *d) 
+{
+    /*成功出队返回1，失败返回0*/
+    if (Q->count == 0)
+    {
+        printf("Queue is emtpy!");
+        return 0;
+    }
+    else
+    {
+        *d = Q->queue[Q->front];
+        Q->count--;
+        return 1;
+    }
+}
+
+
